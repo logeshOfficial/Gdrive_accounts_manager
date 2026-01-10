09d5f175ca33218ab4c04c8cdb5247a72d44ffab
logeshOffical <iamlogeshwaran.info@gmail.com>
2026-01-10 14:30:35 +0530

    "undo main"

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# import streamlit as st
-
-# # 🚦 SAFE CHECK
-# if not st.session_state.get("drive_ready", False):
-#     st.switch_page("pages/load_files_from_gdrive.py")
-    
-# st.title("Home")
-
-# if st.button("Drive Manager"):
-#     st.switch_page("pages/load_files_from_gdrive.py")
-
-# if st.button("Chat Bot"):
-#     st.switch_page("pages/chat_bot.py")
-
 import streamlit as st
-st.write("OAuth-free app running ✅")
+
+# 🚦 SAFE CHECK
+if not st.session_state.get("drive_ready", False):
+    st.switch_page("pages/load_files_from_gdrive.py")
+    
+st.title("Home")
+
+if st.button("Drive Manager"):
+    st.switch_page("pages/load_files_from_gdrive.py")
+
+if st.button("Chat Bot"):
+    st.switch_page("pages/chat_bot.py")
