edbae9691f6490ded736061945a077434982b3d1
logeshOffical <iamlogeshwaran.info@gmail.com>
2026-01-10 17:26:14 +0530

    "dassa"

--- pages/load_files_from_gdrive.py
+++ pages/load_files_from_gdrive.py
@@ -262,11 +262,11 @@ def start_processing():
                         del df
                         gc.collect()
                 
-            media = MediaFileUpload(local, resumable=False)
+            media = MediaFileUpload(local, mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", resumable=True)
             if existing:
-                st.session_state.drive_manager.drive_execute(st.session_state.drive_manager.service.files().update(fileId=existing[0]["id"], media_body=media))
+                st.session_state.drive_manager.drive_execute(st.session_state.drive_manager.service.files().update(fileId=existing[0]["id"], media_body=media), supportsAllDrives=True)
             else:
-                st.session_state.drive_manager.drive_execute(st.session_state.drive_manager.service.files().create(body={"name": fname, "parents": [output_id]}, media_body=media))
+                st.session_state.drive_manager.drive_execute(st.session_state.drive_manager.service.files().create(body={"name": fname, "parents": [output_id]}, media_body=media), supportsAllDrives=True)
 
             import shutil
             shutil.rmtree(tmp_dir, ignore_errors=True)
