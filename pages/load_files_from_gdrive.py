27059503bb5509900eb70239eca5f5e93aa7f8c6
logeshOffical <iamlogeshwaran.info@gmail.com>
2026-01-10 16:39:30 +0530

    "updated"

--- pages/load_files_from_gdrive.py
+++ pages/load_files_from_gdrive.py
@@ -299,8 +299,8 @@ if "drive_manager" not in st.session_state:
     # Step 1: Root folder
     status.info("📁 Checking root folder...")
     root_folder_id = st.session_state.drive_manager.resolve_folder_id(PARENT_FOLDER)
-    input_docs_folder_id = st.session_state.drive_manager.resolve_folder_id(INPUT_DOCS)
-    folder_b_id = st.session_state.drive_manager.get_child_folder_id(st.session_state.drive_manager.service, INPUT_DOCS, root_folder_id)
+    # input_docs_folder_id = st.session_state.drive_manager.resolve_folder_id(INPUT_DOCS)
+    input_docs_folder_id = st.session_state.drive_manager.get_child_folder_id(st.session_state.drive_manager.service, INPUT_DOCS, root_folder_id)
 
     progress.progress(25)
     st.info(f"Processing files from folder: {PARENT_FOLDER}")
@@ -327,7 +327,8 @@ drive_manager = st.session_state.drive_manager
 if st.button("Start Invoice Processing"):
     invoice_processor = InvoiceProcessor()
     root_folder_id = drive_manager.resolve_folder_id(PARENT_FOLDER)
-    input_docs_folder_id = drive_manager.resolve_folder_id(INPUT_DOCS)
+    # input_docs_folder_id = drive_manager.resolve_folder_id(INPUT_DOCS)
+    input_docs_folder_id = drive_manager.get_child_folder_id(drive_manager.service, INPUT_DOCS, root_folder_id)
     DRIVE_DIRS = st.session_state.drive_dirs
     output_id = st.session_state.drive_dirs["output"]
     start_processing()
