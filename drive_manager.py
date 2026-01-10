3d5363ee91ffe6d4f54196edda2a91ee82de2f26
logeshOffical <iamlogeshwaran.info@gmail.com>
2026-01-10 16:36:27 +0530

    "update"

--- drive_manager.py
+++ drive_manager.py
@@ -23,7 +23,7 @@ class DriveManager:
             st.error("Error ocured while loading Google Drive credentials on secrets. Please contact the administrator.")
             st.stop()
             
-    def get_child_folder_id(service, folder_name, parent_id):
+    def get_child_folder_id(self, service, folder_name, parent_id):
         query = (
             f"name='{folder_name}' and "
             f"mimeType='application/vnd.google-apps.folder' and "
